9273873f1e006dc944654a34774fd36dd632554a
Vitor França <vitufr@gmail.com>
2015-07-16 00:11:17 -0300

    reindent

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var modRewrite = require('connect-modrewrite');
 
 var app = express();
 
-livereload(app, config={ watchDir: "." });
+livereload(app, config = {
+    watchDir: "."
+});
 // <script>
 //   document.write('<script src="http://' + (location.host || 'localhost')
 //     .split(':')[0] + ':35729/livereload.js"></' + 'script>')
@@ -20,8 +22,10 @@ app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png$ /index.html [L]']));
 app.use('/', express.static(path.join(__dirname, '')));
 // app.use('/', express.static(path.join(__dirname, 'bower_components')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
