6da87045f60d4cd314d9978d3bfa741e923e0866
Vitor França <vitufr@gmail.com>
2015-07-16 00:35:46 -0300

    npm scripts to compile all js into one

--- testCtrl.js
+++
@@ -1,5 +0,0 @@
-app.controller('testCtrl', function($scope, characters, skills, classes) {
-    $scope.characters = characters.all;
-    $scope.skills = skills.all;
-    $scope.classes = classes.all;
-});
