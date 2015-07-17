f02bb916bbbdb2079ea12084822f8c4e7cc65d2a
Vitor França <vitufr@gmail.com>
2015-07-17 01:50:45 -0300

    showing all possible skills for a character

--- app/components/character.js
+++ app/components/character.js
@@ -5,12 +5,16 @@ app.directive('character', function() {
         replace: false
     };
 })
-    .controller('characterComponentCtrl', function($scope, characters, $stateParams) {
+    .controller('characterComponentCtrl', function($scope, characters, $stateParams, classes) {
         characters.get($stateParams.name).then(function(data) {
             $scope.character = data;
         });
 
         characters.getFullClassSet($stateParams.name).then(function(data) {
             $scope.classSet = data;
+        });
+
+        characters.getFullSkillSet($stateParams.name).then(function(data) {
+            $scope.skillSet = data;
         });
     });
