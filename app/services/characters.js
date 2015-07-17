8f385a4c472ccaa115aa3a40dfb7b300fa07c2c3
Vitor França <vitufr@gmail.com>
2015-07-17 01:27:09 -0300

    get all possible classes for a character

--- app/services/characters.js
+++ app/services/characters.js
@@ -1,4 +1,4 @@
-app.service('characters', function(firebase) {
+app.service('characters', function(firebase, classes, $q) {
     var self = this;
 
     self.all = firebase.getList('characters');
@@ -12,9 +12,24 @@ app.service('characters', function(firebase) {
     };
 
     self.get = function(name) {
-        return self.all[name] ?
-            self.all[name] :
-            firebase.get('characters/' + name);
+        return self.all.$loaded()
+            .then(function(data) {
+                return data[name];
+            });
+    };
+
+    self.getFullClassSet = function(name) {
+        return self.get(name).then(function(character) {
+            var promises = character.classSet.map(function(className) {
+                return classes.get(className);
+            });
+
+            return $q.all(promises).then(function(klasses) {
+                return _.chain(klasses.map(function(klass) {
+                    return [klass.name, klass.promote];
+                })).flatten().compact().uniq().value();
+            });
+        });
     };
 
     return self;
