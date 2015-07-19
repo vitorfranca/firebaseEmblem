4421de947c021880f02ece72a2d00950958e4014
Vitor França <vitufr@gmail.com>
2015-07-19 15:08:20 -0300

    removing useless controller

--- app/states/characters/details/index.js
+++ app/states/characters/details/index.js
@@ -5,9 +5,6 @@ module.exports = angular.module('fire-emblem')
         $stateProvider
             .state('root.characters.details', {
                 url: "/name/:name",
-                templateUrl: "/app/states/characters/characters.html",
-                controller: function() {
-                    console.log('characters details');
-                }
+                templateUrl: "/app/states/characters/characters.html"
             });
     });
