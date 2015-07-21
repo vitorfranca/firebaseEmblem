cbfbc1d8938023a7bba419075bf7bf9d4f0b61fa
Vitor França <vitufr@gmail.com>
2015-07-20 22:20:16 -0300

    moving character component to character state

--- app/states/characters/details/index.js
+++ app/states/characters/details/index.js
@@ -1,10 +1,25 @@
 'use strict';
 
+function characterCtrl($scope, characters, $stateParams, classes) {
+    characters.get($stateParams.name).then(function(data) {
+        $scope.character = data;
+    });
+
+    characters.getFullClassSet($stateParams.name).then(function(data) {
+        $scope.classSet = data;
+    });
+
+    characters.getFullSkillSet($stateParams.name).then(function(data) {
+        $scope.skillSet = data;
+    });
+}
+
 module.exports = angular.module('fire-emblem')
     .config(function($stateProvider) {
         $stateProvider
             .state('root.characters.details', {
                 url: "/name/:name",
-                templateUrl: "/app/states/characters/details/character.html"
+                templateUrl: "/app/states/characters/details/character.html",
+                controller: characterCtrl
             });
     });
