0e5ad0cce54f1eef04e6fcc3150d950ae8057514
Vitor França <vitor.franca@izzui.com>
2015-08-27 18:58:35 -0300

    jjjkljlkj

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var watch = require('gulp-watch');
 var cssmin = require("gulp-cssmin");
 var concat = require("gulp-concat");
 var stripCssComments = require('gulp-strip-css-comments');
+var sass = require('gulp-sass');
 
 var css = [
     './app/css/**/*.css'
@@ -15,11 +16,23 @@ gulp.task('minify-css', function() {
     //     all: true
     // }))
     // .pipe(cssmin())
-        .pipe(gulp.dest('./assets/css/'));
+    .pipe(gulp.dest('./assets/css/'));
 });
 
-gulp.task('default', ['minify-css']);
+gulp.task('default', ['css']);
+gulp.task('css', ['sass', 'minify-css']);
+// gulp.task('default', ['minify-css']);
 
 gulp.task('watch-css', function() {
     gulp.watch(css, ['minify-css']);
+});
+
+gulp.task('sass', function() {
+    gulp.src('./app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./scss/**/*.scss', ['sass']);
 });
